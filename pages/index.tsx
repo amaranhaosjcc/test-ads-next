777ed466e7828e14704ed49868ecdeb2655b7075
amaranhaosjcc <70663992+amaranhaosjcc@users.noreply.github.com>
2020-12-17 14:26:36 -0300

    first

---
+++ pages/index.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <script 
+          data-ad-client="ca-pub-7020394555307984" 
+          async 
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js">
+        </script>
+      </Head>
+
+      <main>
+        <h1>Hello World</h1>
+      </main>
+
+    
+    </>
+  )
+}
